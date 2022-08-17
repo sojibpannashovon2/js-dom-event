15762d0c707ae5121101d8ad7f8367beacac5ff9
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-17 20:33:40 +0600

    Taking input text

--- javaScript/event.js
+++ javaScript/event.js
@@ -23,3 +23,27 @@ const purpleButtonFunction = document.getElementById('purple-button');
 purpleButtonFunction.onclick = function purpleButton() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option -4 By function with addEventListener
+
+const pinkButtonFunction = document.getElementById('pink-button');
+
+pinkButtonFunction.addEventListener('click', pinkButton);
+
+function pinkButton() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// Another option of 4 with addEventLisener part-1
+
+const greenButtonFunction = document.getElementById('green-button');
+
+greenButtonFunction.addEventListener('click', function greenButton() {
+    document.body.style.backgroundColor = 'green';
+});
+
+// Another option of 4 with addEventLisener part-2
+
+document.getElementById('orange-button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'orange';
+})
